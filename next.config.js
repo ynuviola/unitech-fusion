1a1ff44258d4110eb68809e9a82b03e4269f7c22
YaselNuviola <nuviola86@gmail.com>
2025-05-17 16:58:24 -0300

    corrijo otros erroes nuevos2

--- next.config.js
+++ next.config.js
@@ -3,13 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'www.unitechfusion.com', 'unitechfusion.com', 'jsofkoyudgrjwksgdinq.supabase.co'],
+    domains: [
+      'localhost',
+      'www.unitechfusion.com',
+      'unitechfusion.com',
+      'jsofkoyudgrjwksgdinq.supabase.co',
+    ],
     unoptimized: process.env.NODE_ENV === 'production',
   },
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   trailingSlash: true,
-  // Configuración condicional para exportación estática solo en producción
-  ...(process.env.EXPORT_STATIC === 'true' ? { output: 'export' } : {}),
+  // ❌ Eliminar esta parte ↓
+  // ...(process.env.EXPORT_STATIC === 'true' ? { output: 'export' } : {}),
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
